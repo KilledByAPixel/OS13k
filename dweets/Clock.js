2075e1f4822d30171daf16b6c0f6bcfc83c2fa45
DaSpider <17607856+DaSpider@users.noreply.github.com>
2020-06-04 21:30:02 +0300

    Simple analog clock

---
+++ dweets/Clock.js
@@ -0,0 +1,2 @@
+for(z=(c.width|=0)/4,b=Date().slice(16,24).split(':'),x.translate(z*2,z),i=60;i--;x.rotate(-.105),x.fillRect(0,-!h*z+35,1+(h||i%5==0)*5,-30-h*z))
+h=.5*(i==b[0]%12*5)+(i==b[1])+(i==b[2])
