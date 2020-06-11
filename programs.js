28a4c6895ab1749ab2d922fbab9a4bec6c5a0b04
Frank Force <frankforce@gmail.com>
2020-06-11 16:18:52 -0500

    Add files via upload

--- programs.js
+++ programs.js
@@ -6,7 +6,7 @@ const programs =
 {src:'https://js13kgames.com', name:'JS13k Website', icon:'<b><span style=font-family:currier;color:#b33>13k'},
 {name:'System', icon:'⚙️', folder:
 [
-    {src:'system/trophyCase.html?8', icon:'🏆', sticky:1},
+    {src:'system/trophyCase.html?8', icon:'🏆'},
     {src:'system/clock.dweet.js?8', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1},
     {src:'system/shell.html?1',icon:'<span style=color:#0f0;background:#000>&nbsp;>&nbsp;',  reload:1},
     {src:'system/test.html?11', reload:1, code:1, width:600, height:400, sticky:1},
