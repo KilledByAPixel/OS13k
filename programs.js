0e2addacd0b68f11725fd6ac90effc8451a2cdd6
Frank Force <frankforce@gmail.com>
2020-06-11 13:57:32 -0500

    camel case name detection

--- programs.js
+++ programs.js
@@ -1,62 +1,62 @@
 const programs = 
 [
 {name:'Close All', icon:'❌',},
-{name:'Help', icon:'<b><span style=color:#0f0>?', src:'help.html?15', width:380, height:380},
-{name:'More Info', icon:'👨‍💻', src:'info.html?9'},
+{icon:'<b><span style=color:#0f0>?', src:'Help.html?15', width:380, height:380},
+{icon:'👨‍💻', src:'MoreInfo.html?9'},
 {name:'JS13k Website', icon:'<b><span style=font-family:currier;color:#b33>13k', src:'https://js13kgames.com'},
 {name:'System', icon:'⚙️', folder:
 [
-    {name:'Trophy Case', icon:'🏆', src:'system/trophies.html?8', sticky:1},
-    {name:'Clock', icon:'🕰️', src:'system/clock.dweet.js?7', width:200, height:200, reload:0, sleep:0, full:0, sticky:1},
-    {name:'Shell', icon:'<span style=color:#0f0;background:#000>&nbsp;>&nbsp;', src:'system/shell.html?1', reload:1},
-    {name:'Test', src:'system/test.html?11', reload:1, code:1, width:600, height:400, sticky:1},
+    {icon:'🏆', src:'system/TrophyCase.html?8', sticky:1},
+    {icon:'🕰️', src:'system/Clock.dweet.js?7', width:200, height:200, reload:0, sleep:0, full:0, sticky:1},
+    {icon:'<span style=color:#0f0;background:#000>&nbsp;>&nbsp;', src:'system/Shell.html?1', reload:1},
+    {src:'system/Test.html?11', reload:1, code:1, width:600, height:400, sticky:1},
     {name:'Test Folder', icon:'📁', folder:
     [
-        {name:'Hello!', icon:'✌️😄', src:'system/test.html?11', width:600, height:400},
+        {name:'Hello!', icon:'✌️😄', src:'system/Test.html', width:600, height:400},
     ]},
 ]},
 {name:'Tools', icon:'🔧', folder:
 [
-    {name:'Sticky Note', icon:'✍️', src:'tools/stickyNote.html?2', width:300, height:264, full:0, sticky:1},
-    {name:'Photo Booth', icon:'📸', src:'tools/photoBooth.html', width:1400, height:550 },
-    {name:'Unicode Toys', icon:'𝖀', author:'Xem', src:'tools/unicodeToys.html', width:500, height:800},
-    {name:'Mini Shadertoy', icon:'𝓢', src:'tools/miniShadertoy.html', width:340, height:400},
-    {name:'Text Editor', icon:'📝', src:'tools/textEditor.html'},
-    {name:'ZzFX', icon:'<b>ℤℤ', src:'https://zzfx.3d2k.com', height:880},
-    {name:'CapJS', icon:'𝓒𝓪𝓹', src:'https://capjs.3d2k.com', height:750},
-    {name:'Dweet View', icon:'<span style=color:#f00;background:#000><b>&nbsp;III&nbsp;', src:'https://dweetview.3d2k.com'}
+    {icon:'✍️', src:'tools/StickyNote.html?2', width:300, height:264, full:0, sticky:1},
+    {icon:'📸', src:'tools/PhotoBooth.html', width:1400, height:550 },
+    {icon:'𝖀', author:'Xem', src:'tools/UnicodeToys.html', width:500, height:800},
+    {icon:'𝓢', src:'tools/MiniShadertoy.html', width:340, height:400},
+    {icon:'📝', src:'tools/TextEditor.html'},
+    {name:'ZzFX', icon:'<b>ℤℤ', src:'https://ZzFX.3d2k.com', width:1280, height:880},
+    {name:'CapJS', icon:'𝓒𝓪𝓹', src:'https://CapJS.3d2k.com', width:1280, height:750},
+    {icon:'<span style=color:#f00;background:#000><b>&nbsp;III&nbsp;', src:'https://DweetView.3d2k.com'}
 ]},
 {name:'Games', icon:'🎮', folder:
 [
-    {name:'Bounce Back', icon:'❤️', src:'https://bounceback.3d2k.com', author:'Frank Force'},
-    {name:'Swatch', icon:'🌈', src:'games/swatch.html?1', author:'Nicholas Ortenzio', width:320, height:340},
-    {name:'Lava Rush', icon:'🌋', author:'Jeremy Burns', src:'games/lavaRush.html?4', width:640, height:400},
-    {name:'Sn1ke', icon:'👀', author:'Codegolf Team', src:'games/sn1ke.html', width:450, height:330},
-    {name:'Tetris', icon:'<span style=font-size:10>▀█▀', author:'Veubeke', src:'games/tetris.html', width:340, height:430, reload:1},
-    {name:`Queen's Gambit`, icon:'<span style=color:#f0f>♛', src:'games/queensGambit.html?4', reload:1},
-    {name:'Free Cell', icon:'♠️', src:'games/freeCell.html', width:800, height:900},
-    {name:'Digit Dilemma', icon:'<span style=font-family:monospace>☻', src:'games/digitDilemma.html?2'},
-    {name:'Hue Jumper', icon:'🌲', src:'games/hueJumper.html', reload:1},
+    {icon:'❤️', src:'https://BounceBack.3d2k.com', author:'Frank Force'},
+    {icon:'🌈', src:'games/Swatch.html?1', author:'Nicholas Ortenzio', width:320, height:340},
+    {icon:'🌋', author:'Jeremy Burns', src:'games/LavaRush.html?4', width:640, height:400},
+    {icon:'👀', author:'Codegolf Team', src:'games/Sn1ke.html', width:450, height:330},
+    {icon:'<span style=font-size:10>▀█▀', author:'Veubeke', src:'games/Tetris.html', width:340, height:430, reload:1},
+    {icon:'<span style=color:#f0f>♛', src:'games/QueensGambit.html?4', reload:1},
+    {icon:'♠️', src:'games/FreeCell.html', width:800, height:900},
+    {icon:'<span style=font-family:monospace>☻', src:'games/DigitDilemma.html?2'},
+    {icon:'🌲', src:'games/HueJumper.html', reload:1},
 ]},
 {name:'Music', icon:'🎶', folder:
 [
-    {name:'Min Bytes', icon:'<span style=color:#f00>𝓜', width:500, height:500, src:'music/minBytes.html?5'},
+    {icon:'<span style=color:#f00>𝓜', width:500, height:500, src:'music/MinBytes.html?5'},
 ]},
 {name:'Toys', icon:'🤖', folder:
 [
-    {name:'ZzFX Soundboard', icon:'𝐙𝐙', src:'toys/zzfxSoundBoard.html?1', width:700, height:420},
-    {name:'ZzArt Landscape', icon:'𝓩', src:'toys/zzart.shader.txt?3'},
-    {name:'Yin Yangs', icon:'☯️', src:'toys/infiniteYinYangs.shader.txt?2'},
-    {name:'Vogel Spiral', icon:'🌀', src:'toys/vogelSpiral.shader.txt?2'},
+    {name:'ZzFX Sound Board', icon:'𝐙𝐙', src:'toys/ZzFXSoundBoard.html?1', width:700, height:420},
+    {name:'ZzArt Landscape', icon:'𝓩', src:'toys/ZzArtLandscape.shader.txt?3'},
+    {icon:'☯', src:'toys/InfiniteYinYangs.shader.txt?2'},
+    {icon:'🌀', src:'toys/VogelSpiral.shader.txt?2'},
     {name:'Meta OS13k', icon:'𝐎𝐒', src:'index.html'},
 ]},
 {name:'Dweets', icon:'<b>III', folder:
 [
-    {name:'Black Hole', icon:'🌌', src:'dweets/blackHole.dweet.js'},
-    {name:'Underwater Cavern', icon:'🌊', author:'Pavel', src:'dweets/underwaterCavern.dweet.js'},
-    {name:'City Traffic', icon:'🚌', author:'Tomxor', src:'dweets/cityTraffic.dweet.js'},
-    {name:'Train Set', icon:'🚂', author:'jylikangas', src:'dweets/trainSet.dweet.js'},
-    {name:'Automatic Breakout', icon:'●', src:'dweets/breakout.dweet.js'},
-    {name:'Bogus Roads', icon:'🛣️', src:'dweets/bogusRoads.dweet.js'},
+    {icon:'🌌', src:'dweets/BlackHole.dweet.js'},
+    {icon:'🌊', author:'Pavel', src:'dweets/UnderwaterCavern.dweet.js'},
+    {icon:'🚌', author:'Tomxor', src:'dweets/CityTraffic.dweet.js'},
+    {icon:'🚂', author:'jylikangas', src:'dweets/TrainSet.dweet.js'},
+    {icon:'●', src:'dweets/AutomaticBreakout.dweet.js'},
+    {icon:'🛣️', src:'dweets/BogusRoads.dweet.js'},
 ]},
 ];
