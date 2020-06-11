5d5dbd214fe56aae59391b6a00a47f8ae2461fa9
Frank Force <frankforce@gmail.com>
2020-06-11 15:53:53 -0500

    better clock, fix startup glitch

--- programs.js
+++ programs.js
@@ -7,7 +7,7 @@ const programs =
 {name:'System', icon:'⚙️', folder:
 [
     {src:'system/trophyCase.html?8', icon:'🏆', sticky:1},
-    {src:'system/clock.dweet.js?7', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1},
+    {src:'system/clock.dweet.js?8', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1},
     {src:'system/shell.html?1',icon:'<span style=color:#0f0;background:#000>&nbsp;>&nbsp;',  reload:1},
     {src:'system/test.html?11', reload:1, code:1, width:600, height:400, sticky:1},
     {name:'Test Folder', icon:'📁', folder:
