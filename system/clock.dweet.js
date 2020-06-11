bbe110e2a821218c61fb99f5a79ffab60afa8199
Frank Force <frankforce@gmail.com>
2020-06-10 19:44:07 -0500

    clock and cleanup icon stuff

---
+++ system/clock.dweet.js
@@ -0,0 +1,6 @@
+c.style.filter='sepia()invert('
+d=Date(x.translate(z=(c.width|=0)/2,z)).slice(16,24).split(':')
+for(i=60;i--;C(i/60,1+!(i%5),9,90))C=(V,W,L,H=0)=>x.rotate(A=Math.PI*(1-2*V),x.fillRect(-W,H-9,2*W,L,x.rotate(-A)))
+C(d[0]%12/12,3,59)
+C(d[1]/60,2,85)
+C(d[2]/60,1,99)
