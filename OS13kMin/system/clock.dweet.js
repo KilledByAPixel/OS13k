4bc5ada9b304b3c8db7e31dc78fae9f64c2b6d02
Frank Force <frankforce@gmail.com>
2020-06-12 16:21:06 -0500

    os13k min
    min 9.59k zipped - 8.48 KB when packed together

---
+++ OS13kMin/system/clock.dweet.js
@@ -0,0 +1,24 @@
+D=new Date()+''
+if(c.title==D)return
+c.title=D
+c.width=c.height=200
+x.fillStyle='#111'
+x.fillRect(0,0,2e3,2e3)
+x.translate(99,99)
+x.setLineDash([z=Math.PI/2,z*5])
+x.lineWidth=3
+x.arc(0,0,90,-z/60,7)
+x.strokeStyle=x.fillStyle='#fff'
+x.stroke()
+x.setLineDash([z*=3,z*9])
+x.lineWidth=9
+x.stroke()
+L=(P,W,L)=>
+x.rotate(a=Math.PI*(1-2*P),
+x.fillRect(-W/2,-6,W,L,
+x.rotate(-a)))
+D=D.slice(16,24).split`:`
+L(D[0]%12/12+D[1]/720,6,59)
+L(D[1]/60,3,85)
+x.fillStyle='#f00'
+L(D[2]/60,2,99)
