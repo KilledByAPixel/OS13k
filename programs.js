e38d3b04732cd2b76319a0e684a4d7bbc03dfd85
Frank Force <frankforce@gmail.com>
2020-06-13 17:21:55 -0500

    fix touch & trophies

--- programs.js
+++ programs.js
@@ -5,7 +5,7 @@ const programs =
 {src:'moreInfo.html?10', icon:'👨‍💻'},
 {name:'System', icon:'⚙️', folder:
 [
-    {src:'system/trophyCase.html?10', icon:'🏆', width:680, sticky:1, help:'All your trophies are shown here.\nTotal trophy count is displayed in the taskbar.\n\nPlay games and experiment to win more trophies!'},
+    {src:'system/trophyCase.html?11', icon:'🏆', width:680, sticky:1, help:'All your trophies are shown here.\nTotal trophy count is displayed in the taskbar.\n\nPlay games and experiment to win more trophies!'},
     {src:'system/clock.dweet.js?13', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1, resize:0},
     {src:'system/shell.html?8',icon:'<span style=color:#0f0;background:#000;font-family:monospace>&nbsp;>&nbsp;', sticky:1},
     {src:'system/test.html?15', code:1, width:600, height:400, sticky:1},
