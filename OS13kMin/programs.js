0602b5f4175cb2e29fc31e6907b6f0fa1cd8d138
Frank Force <frankforce@gmail.com>
2020-06-19 12:41:58 -0500

    Fixed Minify - 11.7kb

--- OS13kMin/programs.js
+++ OS13kMin/programs.js
@@ -1,1 +1,14 @@
-const programs=[{name:"Close All",icon:"❌"},{src:"help.html?16",icon:"<b><span style=color:#0f0>?",width:380,height:240,g:"Check here to see help when available.",reload:0},{name:"System",icon:"⚙️",folder:[{src:"system/trophyCase.html?8",icon:"🏆",width:680,g:"All your trophies are shown here.\nTotal trophy count is displayed in the taskbar.",reload:0},{src:"system/clock.dweet.js?11",icon:"🕰️",width:200,height:200,reload:0,sleep:0,full:0,sticky:1},{src:"system/shell.html?1",icon:"<span style=color:#0f0;background:#000>&nbsp;>&nbsp;"},{src:"system/test.html?12",code:1,width:600,height:400,sticky:1}]}]
+const programs = 
+[
+{name:'Close All', icon:'❌',},
+{src:'help.html?19', icon:'<b><span style=color:#0f0>?', width:380, height:400, help:'Check here to see help when available.\nSome programs have a option to show code instead.\n\nHave fun! ✌️😄', reload:0},
+{src:'moreInfo.html?12', icon:'👨‍💻'},
+{name:'System', icon:'⚙️', folder:
+[
+    {src:'system/console.html?63',icon:'<span style=color:#0f0;background:#000;font-family:monospace>JS>'},
+    {src:'system/trophyCase.html?24', icon:'🏆', sticky:1, reload:0},
+    {src:'system/wordList.html?10', icon:'📕', sticky:1, reload:0},
+    {src:'system/clock.dweet.js?15', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1, resize:0},
+    {src:'system/test.html?22', code:1, width:600, height:400, sticky:1}
+]},
+]
--- programs.js
+++ programs.js
@@ -6,9 +6,9 @@ var programs =
 {name:'System', icon:'⚙️', folder:
 [
     {src:'system/console.html?63',icon:'<span style=color:#0f0;background:#000;font-family:monospace>JS>'},
-    {src:'system/trophyCase.html?22', icon:'🏆', width:680, sticky:1, reload:0},
-    {src:'system/wordList.html?9', icon:'📕', sticky:1, reload:0},
-    {src:'system/clock.dweet.js?14', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1, resize:0},
+    {src:'system/trophyCase.html?24', icon:'🏆', sticky:1, reload:0},
+    {src:'system/wordList.html?10', icon:'📕', sticky:1, reload:0},
+    {src:'system/clock.dweet.js?15', icon:'🕰️', width:200, height:200, reload:0, sleep:0, full:0, sticky:1, resize:0},
     {src:'system/test.html?22', code:1, width:600, height:400, sticky:1},
     {name:'Test Folder', icon:'📁', folder:
     [
