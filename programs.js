9dc81da2c26125847694c201fda5cbca1fadd9f8
Frank Force <frankforce@gmail.com>
2020-06-20 18:30:33 -0500

    cut author

--- programs.js
+++ programs.js
@@ -1,9 +1,3 @@
-// ==ClosureCompiler==
-// @output_file_name default.js
-// @compilation_level ADVANCED_OPTIMIZATIONS
-// @language_out ECMASCRIPT_2019
-// ==/ClosureCompiler==
-
 // flags
 var sticky  = 2**0; // always save position and if open
 var reload  = 2**1; // show reload button
@@ -21,64 +15,64 @@ var defaultWidth = 720, defaultHeight = 405; // 16:9 aspect
 
 var programs = 
 [
-[,'❌',,,,,'Close All'],
-['help.html','<b><span style=color:#0f0>?',380,400,full|resize,,,'Check here to see help when available.\nSome programs have a option to show code instead.\n\nHave fun! ✌️😄'],
+[,'❌',,,,'Close All'],
+['help.html','<b><span style=color:#0f0>?',380,400,full|resize,,'Check here to see help when available.\nSome programs have a option to show code instead.\n\nHave fun! ✌️😄'],
 ['moreInfo.html','👨‍💻'],
-[,'⚙️',,,,,'System',,
+[,'⚙️',,,,'System',,
 [
     ['system/test.html',,,,resize|code|sticky],
     ['system/trophyCase.html','🏆',,,resize|sticky],
     ['system/wordList.html','📕',,,resize|sticky],
     ['system/clock.dweet.js','🕰️',200,200,sticky|code|awake],
-    [,'📁',,,,,'Test Folder',,
+    [,'📁',,,,'Test Folder',,
     [
         ['index.html','✌️😄',600,400,,,'Meta OS13k'],
     ]],
 ]],
-[,'🛠️',,,,,'Apps',,
+[,'🛠️',,,,'Apps',,
 [
     ['apps/console.html','<span style=color:#0f0;background:#000;font-family:monospace>JS>'],
     ['apps/stickyNote.html','✍️',300,263,sticky|reload],
     ['apps/filters.html','✨',260,60,reload],
     ['apps/photoBooth.html','📸',1400,550],
-    ['apps/unicodeToys.html','𝖀',500,800,,'Xem'],
+    ['apps/unicodeToys.html','𝖀',500,800],
     ['apps/miniShadertoy.html','𝓢',340,400],
     ['apps/textEditor.html','📝'],
     ['https://dweetView.3d2k.com','&nbsp;III&nbsp'],
 ]],
-[,'🎮',,,,,'Games',,
+[,'🎮',,,,'Games',,
 [
-    ['https://bounceBack.3d2k.com','❤️',,,full|resize,'Frank Force'],
-    ['games/swatch.html','🌈',320,340,reload,'Nicholas Ortenzio'],
-    ['games/lavaRush.html','🌋',640,400,,'Jeremy Burns'],
-    ['games/sn1ke.html','👀',400,280,,'Codegolf Team'],
-    ['games/tetris.html','<span style=font-size:10>▀█▀',210,380,reload,'Veubeke'],
+    ['https://bounceBack.3d2k.com','❤️',,,full|resize],
+    ['games/swatch.html','🌈',320,340,reload],
+    ['games/lavaRush.html','🌋',640,400],
+    ['games/sn1ke.html','👀',400,280],
+    ['games/tetris.html','<span style=font-size:10>▀█▀',210,380,reload],
     ['games/queensGambit.html','<span style=color:#f0f>♛'],
     ['games/freeCell.html','♠️',800,900,full|reload],
     ['games/hueJumper.html','🌲'],
     ['dweets/bogusRoads.dweet.js','🛣️'],
 ]],
-[,'🎶',,,,,'Music',,
+[,'🎶',,,,'Music',,
 [
     ['music/minBytes.html','<span style=color:#f00>𝓜',450,450],
     ['music/piano.html','🎹',800,300],
     ['music/bach.dweet.js','♫'],
 ]],
-[,'🤖',,,,,'Toys',,
+[,'🤖',,,,'Toys',,
 [
-    ['toys/zzfxSoundBoard.html','𝐙𝐙',700,420,,,'ZzFX Sound Board'],
-    ['toys/zzartLandscape.shader.txt','𝓩',,,,,'ZzFX Landscape'],
+    ['toys/zzfxSoundBoard.html','𝐙𝐙',700,420,,'ZzFX Sound Board'],
+    ['toys/zzartLandscape.shader.txt','𝓩',,,,'ZzFX Landscape'],
     ['toys/infiniteYinYangs.shader.txt','<span style=color:#f00>☯'],
     ['toys/vogelSpiral.shader.txt','🌀'],
 ]],
-[,'<b>III',,,,,'Dweets',,
+[,'<b>III',,,,'Dweets',,
 [
     ['dweets/blackHole.dweet.js','🌌'],
-    ['dweets/underwaterCavern.dweet.js','🌊',,,,'Pavel'],
-    ['dweets/cityTraffic.dweet.js','🚌',,,,'Tomxor'],
-    ['dweets/trainSet.dweet.js','🚂',,,,'jylikangas'],
-    ['dweets/automaticBreakout.dweet.js','■',,,,'●'],
-    ['dweets/colorZoom.dweet.js','❤',,,,'Cantelope'],
-    ['dweets/triFractal.dweet.js','🔺',,,,'Cantelope'],
+    ['dweets/underwaterCavern.dweet.js','🌊'],
+    ['dweets/cityTraffic.dweet.js','🚌'],
+    ['dweets/trainSet.dweet.js','🚂'],
+    ['dweets/automaticBreakout.dweet.js','■'],
+    ['dweets/colorZoom.dweet.js','❤'],
+    ['dweets/triFractal.dweet.js','🔺'],
 ]],
 ]; // programs
