83d0790c01343101778c9a449e89a33df1298861
Frank Force <frankforce@gmail.com>
2020-06-21 19:53:10 -0500

    Add files via upload

--- programs.js
+++ programs.js
@@ -21,7 +21,7 @@ programs =
 ['moreInfo.html','👨‍💻'],
 [,'⚙️',,,,'System',,
 [
-    ['system/settings.html','🎚️',420,180,sticky],
+    ['system/settings.html','🎚️',450,180,sticky],
     ['system/trophyCase.html','🏆',,,full|resize|sticky],
     ['system/wordList.html','📕',,,full|resize|sticky],
     ['system/clock.dweet.js','🕰️',200,200,sticky|code|awake],
