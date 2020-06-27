7f7ede1705f10bcde9f31bc984a53e88d74bde46
Frank Force <frankforce@gmail.com>
2020-06-27 09:22:40 -0500

    Add files via upload

--- music/songs/error.js
+++ music/songs/error.js
@@ -1,5 +1,5 @@
 /**
  * This pattern data is based on a conversion of the loading tune from the game "Sanxion", published by Thalumus. The original song was composed by Rob Hubbard. I have no idea who converted it to MOD format.
  */
-K = 'OS13kMusic,OS13k Test Song Error';
+K = 'OS13kMusicS,OS13k Test Song Error';
 if (localStorage[K] == undefined) localStorage[K] = JSON.stringify([0]);
