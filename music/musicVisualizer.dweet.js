fc0ace3677af570ff6af20849bfed0dff4b36e8b
Frank Force <frankforce@gmail.com>
2020-06-30 18:37:15 -0500

    music analyser improvemnts

--- music/musicVisualizer.dweet.js
+++ music/musicVisualizer.dweet.js
@@ -1,2 +1,2 @@
 x.fillRect(0,0,c.width|=0,2e3)
-for(i=0;++i<32;x.fill())x.arc(960,540,59*OS13k.GetAnalyzer(x.beginPath(x.fillStyle=`hsl(${i*7-120},99%,50%,.1)`)).getContext`2d`.getImageData(i,31,1,1).data[0]/(i+9),0,7)
+for(i=0;++i<32;x.fill())x.arc(960,540,1e4*OS13k.GetAnalyserData(x.beginPath(x.fillStyle=`hsl(${i*7-120},99%,50%,.1)`))[i]/(i+9),0,7)
