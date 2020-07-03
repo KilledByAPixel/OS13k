08b2697b65df9c094fa3715d49b53a66acc7422d
Frank Force <frankforce@gmail.com>
2020-07-03 00:57:23 -0500

    Add files via upload

--- music/visualizer.dweet.js
+++ music/visualizer.dweet.js
@@ -1,2 +1,2 @@
 x.fillRect(0,0,c.width|=0,2e3)
-for(i=0;++i<32;x.fill())x.arc(960,540,1e4*OS13k.GetAnalyserData(x.beginPath(x.fillStyle=`hsl(${i*7-120},99%,50%,.1)`))[i]/(i+9),0,7)
+for(i=0;++i<32;x.fill(),x.beginPath(x.fillStyle=`hsl(${i*7-120},99%,50%,.1)`))x.arc(960,540,1e4*OS13k.GetAnalyserData(i)/(i+9),0,7)
