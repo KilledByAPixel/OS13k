cc3cabaea3345f962b02221227dfa27071d9de90
Frank Force <frankforce@gmail.com>
2020-07-09 19:25:15 -0500

    new visualizer

--- music/visualizer.dweet.js
+++ music/visualizer.dweet.js
@@ -1,2 +1,7 @@
-x.fillRect(0,0,c.width|=0,2e3)
-for(i=0;++i<32;x.fill(),x.beginPath(x.fillStyle=`hsl(${i*7-120},99%,50%,.1)`))x.arc(960,540,1e4*OS13k.GetAnalyserData(i)/(i+9),0,7)
+x.fillRect(0,0,2e3,2e3)
+t?x.fillStyle='#0003':(h=[],x.lineWidth=b=9)
+x.strokeStyle='#fff'
+for(i=32;i--;x.beginPath(x.strokeStyle=`hsl(${-99-i*9} 99%${9+m/2}%)`,x.stroke()))
+
+for(b+=m=OS13k.GetAnalyserData(i)*199,j=5;j--;)x.arc(960,540,256+(j+1&2?1:-1)*m,a=((j&2)/2+b/4e4-i)*2*Math.PI/32,a)
+x.drawImage(c,-40,-20,2e3,1120)
