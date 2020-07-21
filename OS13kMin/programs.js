66e6ea8b87ca99bf53606052086bf19cae73165e
Frank Force <frankforce@gmail.com>
2020-07-21 14:46:38 -0500

    Add files via upload

--- OS13kMin/programs.js
+++ OS13kMin/programs.js
@@ -1,11 +1,12 @@
 // flags
-const
+var              // change to const for final build
 sticky   = 2**0, // always save position and if open
 reload   = 2**1, // show reload button
-awake    = 2**2, // prevent dim and pause when not active
+awake    = 2**2, // prevent sleep and dim when not active
 full     = 2**3, // show full screen button
 resize   = 2**4, // show resize buttons (apect ratio maintained)
 code     = 2**5, // show code button (cant show code if help is set)
+close    = 2**6, // show code button (cant show code if help is set)
 
 // system flags
 closeAll   = 2**7,         // close all open windows
@@ -22,26 +23,25 @@ defaultWidth = 720, defaultHeight = 405, // 16:9 aspect
 programStubs = 
 [
 [,'❌',,,closeAll,'Close All'],
-['help.html','<b><span style=color:#0f0>?',380,400,sticky,,'Check here to see help when available.\nSome programs have a option to show code instead.\n\nHave fun! ✌️😄'],
+['help.html','<b><span style=color:#0f0>?',380,300,sticky,,'Check here to see help when available.\nSome programs have a option to show code instead.\n\nHave fun! ✌️😄'],
 [,'⚙️',,,,'System',,
 [
     ['system/settings.html','🎚️',470,190,sticky],
     ['system/trophyCase.html','🏆',,,full|resize|sticky],
-    ['system/clock.dweet.js','🕰️',198,198,sticky|code|awake],
-    [,'📁',,,,'Test',,
-    [
-        ['system/systemTest.html',,600,370,defaultFlags|code],
-        ['index.html','✌️😄',,,,'Meta OS13k'],
-    ]],
+    ['system/stickyNote.html','✏️',300,263,sticky|reload,,'Ctrl+B - Bold\nCtrl+I - Italic\nCtrl+U - Underline\n\Reload to clear'],
+    ['system/clock.dweet.js','🕰️',198,198,sticky|code|awake]
 ]],
 [,'🛠️',,,,'Apps',,
 [
-    ['apps/stickyNote.html','✍️',300,263,sticky|reload,,'Ctrl+B - Bold\nCtrl+I - Italic\nCtrl+U - Underline\n\Reload to clear'],
     ['apps/timer.html','⌛',400,139,sticky|reload],
+    ['apps/grapher.html','📈'],
+    ['apps/camera.html','📸',1280,960],
+    ['apps/console.html','<span style=color:#0f0;background:#000;font-family:monospace>JS>']
 ]],
 [,'🎮',,,,'Games',,
 [
     ['games/bogusSlopes.dweet.js','🏂🏻'],
+    ['games/dontFall.dweet.js','🙉'],
 ]],
 [,'🎶',,,,'Music',,
 [
