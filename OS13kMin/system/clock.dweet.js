371a802d9be2baa096f7ae8f9b59d4d0d5a513aa
Frank Force <frankforce@gmail.com>
2020-07-22 11:21:48 -0500

    remove min folder

--- OS13kMin/system/clock.dweet.js
+++
@@ -1,1 +0,0 @@
-D=Date(L=(t,e,l,o=-6)=>x.fillRect(-e/2,o,e,l,x.rotate(-a+(a=Math.PI*(2*t-1)))));if(c.title!=D){document.hasFocus()&&OS13k.PlaySeed(6,.1,1,.01);c.title=D;c.style.background='#111';c.width=198;x.translate(99,99);D=D.slice(16,24).split`:`;for(a=i=0;i++<61;L(i/60,2,i%5?3:8,88))x.fillStyle='#fff';L(D[0]%12/12+D[1]/720,6,59);L(D[1]/60,3,85);x.fillStyle='#f00';L(D[2]/60,2,99)}
