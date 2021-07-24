0ccac9417a8c316961261674845839ebefc8be2c
Frank Force <frankforce@gmail.com>
2021-07-24 10:10:39 -0500

    improve sandbox

--- games/sandbox.dweet.js
+++ games/sandbox.dweet.js
@@ -1,14 +1,22 @@
-// Tiny Sandbox ⌛ Select with mouse wheel... 
-// 1 🌊 Water
-// 2 ⚡️ Lightning Eraser
-// 3 ⛈️ Rain Storm
-// 4 🧱 Wall
-// 5 ⌛️ Sand
-// 6 🌱 Vines
-// 7 🌪️ Sand Storm
-// Right Click 🧼 Erase 
+// Tiny Sandbox ⛱️ - Select with mouse wheel
+// Left click to place - Right click to remove
 
-for(w=c.width=128,i=9216;--i;v==2|v&3*!T[S=i+(i*t^i)%3-1+(v>1|S&1?v&2?-w:w:0)]?T[i]=(T[S]=v)&2?v-2:0:onmousemove=e=>e.buttons?T[w*(e.y*(w/=innerWidth)|0)+e.x*w|0]=e.buttons==1?P:0:0)x.fillRect(i%w,i>>7,1,(v=T[i])/5)
-t?onwheel=e=>P=(P+(e.deltaY<0?1:7)-1)%8+1:P=1
-x.fillText(P,0,9)
+for(w=c.width=128,i=9216;--i;v==2|v&3*!T[S=i+(i*t^i)%3-1+(v>1|S&1?v&2?-w:w:0)]?T[i]=(T[S]=v)&2?v-2:0:0)x.fillRect(i%w,i>>7,1,(v=T[i])/5)
+
+// os13k input
+I = OS13k.Input(window)
+P = t ? I.wheel ? (P+(I.wheel<0?1:6))%7 : P : 4
+if (I.mousedown[0] || I.mousedown[2])
+    T[w*(I.mousey*(w/=innerWidth)|0)+I.mousex*w|0] = I.mousedown[0] ? P+1 : 0
+
+x.fillText([
+'🌊 Water',
+'⚡️ Lightning Eraser',
+'⛈️ Rain Storm',
+'🧱 Wall',
+'⌛️ Sand',
+'🌱 Vines',
+'🌪️ Sand Storm'
+][P],0,10)
+
 // by KilledByAPixel https://www.dwitter.net/d/12312313344
