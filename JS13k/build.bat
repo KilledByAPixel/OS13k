3b427ea9e5f4168f38b142718cfd5351bb5b3af3
Frank Force <frankforce@gmail.com>
2021-08-13 14:38:44 -0500

    uglifyjs

--- JS13k/build.bat
+++ JS13k/build.bat
@@ -1,8 +1,11 @@
 rem SIMPLE BUILD SCRIPT FOR JS13k by FRANK FORCE
 rem minfies and combines index.html and index.js and zips the result
 
+set name=OS13k
+
 rem install closure and advzip globally if necessary
 rem npm install -g google-closure-compiler
+rem npm install -g uglify-js
 rem npm install -g advzip-bin
 
 rem remove old files
@@ -12,6 +15,9 @@ rmdir /s /q build
 
 rem run google closure compiler
 google-closure-compiler --js index.js --externs externs.js --js_output_file build\index.js --compilation_level ADVANCED --language_out ECMASCRIPT_2019 --warning_level VERBOSE --jscomp_off * | more
+
+rem run uglify
+uglifyjs -o build\index.js --compress --mangle -- build\index.js | more
 
 rem remove script tag
 findstr /v "index.js" index.html > build\index.html
@@ -26,7 +32,7 @@ cd build
 advzip -a -4 -i 99 index.zip index.html | more
 
 rem copy zip and remove build folder
-copy index.zip ..\index.zip
+copy index.zip ..\%name%.zip
 copy index.html ..\index.min.html
 cd ..
 rmdir /s /q build
--- JS13k2021/build.bat
+++ JS13k2021/build.bat
@@ -1,19 +1,23 @@
 rem SIMPLE BUILD SCRIPT FOR JS13k by FRANK FORCE
 rem minfies and combines index.html and index.js and zips the result
 
-rem install closure and advzip globally if necessary
-rem npm install -g google-closure-compiler
-rem npm install -g advzip-bin
-
 set name=OS13k
 
+rem install closure and advzip globally if necessary
+rem npm install -g google-closure-compiler
+rem npm install -g uglify-js
+rem npm install -g advzip-bin
+
 rem remove old files
-del %name%.zip
+del index.zip
 del index.min.html
 rmdir /s /q build
 
 rem run google closure compiler
 google-closure-compiler --js index.js --externs externs.js --js_output_file build\index.js --compilation_level ADVANCED --language_out ECMASCRIPT_2019 --warning_level VERBOSE --jscomp_off * | more
+
+rem run uglify
+uglifyjs -o build\index.js --compress --mangle -- build\index.js | more
 
 rem remove script tag
 findstr /v "index.js" index.html > build\index.html
@@ -25,10 +29,10 @@ echo ^</script^> >> build\index.html
 
 rem zip the result
 cd build
-advzip -a -4 -i 99 %name%.zip index.html | more
+advzip -a -4 -i 99 index.zip index.html | more
 
 rem copy zip and remove build folder
-copy %name%.zip ..\%name%.zip
+copy index.zip ..\%name%.zip
 copy index.html ..\index.min.html
 cd ..
 rmdir /s /q build
