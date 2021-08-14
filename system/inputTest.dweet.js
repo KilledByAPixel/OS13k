efb2e9705e1d46112f4295fc3cc412e58e162885
Frank Force <frankforce@gmail.com>
2021-08-13 23:42:52 -0500

    Update inputTest.dweet.js

--- system/inputTest.dweet.js
+++ system/inputTest.dweet.js
@@ -4,7 +4,6 @@ x.font = '4em"'
 x.fillText('x: ' + i.x + ' y: ' + i.y, X=h=79, Y=h)
 x.fillText('keypress:'   + i.keypress.map((k,i)=>k?i:'').filter(k=>k).join`,`,X,Y+=h)
 x.fillText('keydown:'    + i.keydown.map((k,i)=>k?i:'').filter(k=>k).join`,`,X,Y+=h)
-x.fillText('reload: ' + i.reset,X,Y+=h)
 x.fillText('mousex: '    + i.mousex + ' mousey: ' + i.mousey,X,Y+=h)
 x.fillText('mousepress:' + i.mousepress.map((k,i)=>k>0?i:'').filter(k=>k!=='').join`,`,X,Y+=h)
 x.fillText('mousedown:'  + i.mousedown.map((k,i)=>k>0?i:'').filter(k=>k!=='').join`,`,X,Y+=h)
